d22e6f34d586df6cb5bee7c788e65a238764b3d7
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-05 17:32:11 +0430

    Update add-user route to post

--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -4,5 +4,6 @@ const controllers = require("../controllers/UserControllers");
 const router = express.Router();
 
 router.get('/get/:search', controllers.getUsers);
+router.post('/add', controllers.addUser);
 
 module.exports = router;
