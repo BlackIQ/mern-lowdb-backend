9a9df625bdb42cbf8c33626b25985b775435faec
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-05 17:31:35 +0430

    Add addUser method

--- app/controllers/UserControllers.js
+++ app/controllers/UserControllers.js
@@ -1,27 +1,38 @@
 const { join } = require("path");
 const lowDb = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+// const { nanoid } = require("nanoid");
 
 const file = join(__dirname, 'db.json');
 const db = lowDb(new FileSync(file));
 
-const getUsers = async (req, res) => {
+const getUsers = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
     const searchData = req.params.search;
 
-    const data = await db.get("users");
+    const data = db.get("users").value();
 
     res.status(200);
     res.send(data);
 }
 
-const addUser = async (req, res) => {
+const addUser = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
-    const name = req.params.name;
+    const data = req.body;
 
+    const lastUserId = db.get("users").value().length === 0 ? 0 : db.get("users").last().value().id;
 
+    const newUser = {
+        ...data,
+        id: lastUserId + 1,
+    };
+
+    db.get("users").push(newUser).write();
+
+    res.status(200);
+    res.send(newUser);
 }
 
 module.exports = {
