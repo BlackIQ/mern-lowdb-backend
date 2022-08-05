5471b52f37f1aa26b9154aba42bdd267b590ad1c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-05 22:30:39 +0430

    Add delete route

--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -3,9 +3,9 @@ const controllers = require("../controllers/UserControllers");
 
 const router = express.Router();
 
-router.get('/info', controllers.info);
 router.get('/search/:search', controllers.searchUsers);
 router.get('/get/:uid', controllers.getUser);
 router.post('/add', controllers.addUser);
+router.post('/del', controllers.delUser);
 
 module.exports = router;
