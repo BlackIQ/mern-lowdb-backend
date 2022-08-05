c9572401c1286cb9904970c50a9c23ec31761d9c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-05 20:58:25 +0430

    Add info route

--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -3,6 +3,7 @@ const controllers = require("../controllers/UserControllers");
 
 const router = express.Router();
 
+router.get('/info', controllers.info);
 router.get('/search/:search', controllers.searchUsers);
 router.get('/get/:uid', controllers.getUser);
 router.post('/add', controllers.addUser);
