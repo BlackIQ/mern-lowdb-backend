79aa2595e1076bf496649aa24527e4c74e7a5cd9
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-05 22:40:30 +0430

    Add update route

--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -4,8 +4,10 @@ const controllers = require("../controllers/UserControllers");
 const router = express.Router();
 
 router.get('/search/:search', controllers.searchUsers);
+
 router.get('/get/:uid', controllers.getUser);
 router.post('/add', controllers.addUser);
-router.post('/del', controllers.delUser);
+router.delete('/delete', controllers.deleteUser);
+router.put('/del', controllers.updateUser);
 
 module.exports = router;
