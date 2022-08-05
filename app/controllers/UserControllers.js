aff3ab24b1168f748dd1d845d8f9325839d94b91
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-05 16:47:06 +0430

    Working in setup lowdb

--- app/controllers/UserControllers.js
+++ app/controllers/UserControllers.js
@@ -1,11 +1,27 @@
-const getUsers = (req, res) => {
+const { join, dirname } = require("path");
+const lowDb = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const file = join(__dirname, 'db.json');
+
+const db = lowDb(new FileSync(file));
+
+// db.defaults({ notes: {} }).write();
+
+const getUsers = async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
+
+    await db.read();
+
+    // console.log(db)
 
     const searchData = req.params.search;
 
+    const data = await db.data;
+
     res.status(200);
     res.send({
-        data: searchData,
+        data,
     });
 }
 
